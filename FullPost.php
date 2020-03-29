6f6bacb2b98fd7f81ecfd9b1d65521fd2152f176
arthus <arthus8ni@gmail.com>
2020-03-29 20:42:54 +0200

    on 65 now

--- FullPost.php
+++ FullPost.php
@@ -210,7 +210,7 @@ $CommentContent = $DataRows['comment'];
 </div>
 <br>
 <hr>
-<?php } ?>
+<?php } }?>
 
 <!-- end display existing commen -->
 
@@ -257,19 +257,99 @@ $CommentContent = $DataRows['comment'];
 </div>
 <!-- end comments area -->
 </div><!-- end main area -->
+<!-- side  Area -->
+<div class="col-sm-4" style="min-height:40px; background: white;">
+       <div class="card mt-4">
+       <div class="card-body">
+          <img src="images/stratblog.png" class="d-block img-fluid mb-3" alt="">
+            <div class="text-center">
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+   dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+   ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+   fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+   mollit anim id est laborum.
+            </div>
+       </div>
+       </div>
+<br>
+<div class="card">
+<div class="card-header bg-dark text-light">
+<h2 class="lead"> Sign up! </h2>
 
-     <div class="col-sm-4"><!-- side area -->
-
-
+</div>
+    <div class="card-body">
+      <button type="button" class="btn btn-success btn-block text-center text-white" name="buton">join the forum</button>
+       <button type="button" class="btn btn-danger btn-block text-center text-white mb-4" name="buton">join the forum</button>
+       <div class="input-group mb-3">
+       <input type="text" class="form-control" name="" placeholder="Enter email" value="">
+        </div>
+      <div class="input-group-append">
+      <button type="button" class="btn btn-primary btn-block text-center text-white" name="buton">Subscribe now</button>
+      </div>
      </div>
+</div>
+</div>
+<br>
+  <div class="card">
+        <div class="card-header bg-primary text-light">
+     <h2 class="lead"> Categories </h2>
+      </div>
+              <div class='card-body'>
+             <?php
+             global $ConnectingDB;
+             $sql = "SELECT * FROM category ORDER BY id desc";
+             $stmt =  $ConnectingDB->query($sql);
+             while($DataRows = $stmt->fetch()){
+               $CategoryId = $DataRows["id"];
+               $CategoryName = $DataRows["title"];
+
+              ?>
+              <a href="Blog.php?category=<?php echo $CategoryName; ?>">
+              <span class="heading"><?php echo $CategoryName; ?></span>
+              </a>
+              <br>
+            <?php } ?>
+          </div><br>
+<!-- show recent post -->
+
+<div class="card">
+  <div class="card-header bg-info text-white">
+    <h2 class="lead">Recents Posts </h2>
+  </div>
+<div class="card-body">
+ <?php
+ global $ConnectingDB;
+ $sql = "SELECT * FROM posts ORDER BY id desc LIMIT 0,5";
+ $stmt =  $ConnectingDB->query($sql);
+ while($DataRows = $stmt->fetch()){
+   $Id = $DataRows["id"];
+   $Title = $DataRows["title"];
+   $DateTime = $DataRows['datetime'];
+   $Image = $DataRows['image'];
+
+  ?>
+<div class="media">
+ <img src="Uploads/<?php echo htmlentities($Image) ; ?>" class="d-block img-fluid align-self-start" width="90" height="90" alt="">
+  <div class="media-body ml-2">
+    <a href="FullPost.php?id=<?php echo htmlentities($Id); ?>" target="_blank">
+  <h6 class="lead"><?php echo htmlentities($Title); ?></h6>
+    </a>
+  <p class="small"><?php echo htmlentities($DateTime);?> </p>
+  </div>
+
+</div>
+ <hr>
+<?php } ?>
+</div>
+
+
+  </div>
+<!-- end side area -->
+
 
 </div>
 </div>
 <!-- end header-->
-<!-- Main area-->
-
-
-<!-- end main area -->
 
 
 
